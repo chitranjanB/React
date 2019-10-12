5d958ac95728c04b772300f167b11bad1714379b
Chitranjan Babu <chitranjan.pc8@gmail.com>
2019-10-12 15:32:03 +0530

    initial commit

---
+++ react-ant-design-examples/src/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
